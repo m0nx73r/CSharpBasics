229fc467eb0281c7f8154a582b442d183c489b85
Aditya Kumar <naammkyahai@gmail.com>
2024-01-11 18:26:26 +0530

    added functions in seperate file

---
+++ Calculator/CalculatorFunctions.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Calculator
+{
+     class CalculatorFunctions
+    {
+        public static dynamic Add<t1, t2>(t1 a, t2 b)
+        {
+
+            int n1, n2;
+            double d1, d2;
+            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
+            {
+                return n1 + n2;
+            }
+            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
+            {
+                return d1 + d2;
+            }
+            else
+            {
+                return "Please enter numeric value!";
+            }
+
+        }
+
+
+        public static dynamic Subtract<t1, t2>(t1 a, t2 b)
+        {
+            int n1, n2;
+            double d1, d2;
+            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
+            {
+                return n1 - n2;
+            }
+            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
+            {
+                return d1 - d2;
+            }
+            else
+            {
+                return "Please enter numeric value!";
+            }
+        }
+
+        public static dynamic Multiply<t1, t2>(t1 a, t2 b)
+        {
+            int n1, n2;
+            double d1, d2;
+            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
+            {
+                return n1 * n2;
+            }
+            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
+            {
+                return d1 * d2;
+            }
+            else
+            {
+                return "Please enter numeric value!";
+            }
+        }
+
+
+        public static dynamic Divide<t1, t2>(t1 a, t2 b)
+        {
+            int n1, n2;
+            double d1, d2;
+            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
+            {
+                if (n2 != 0)
+                {
+                    return n1 / n2;
+                }
+                else
+                {
+                    return "Second number shouldn't be zero.";
+                }
+            }
+            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
+            {
+                if (d2 != 0)
+                {
+                    return d1 / d2;
+                }
+                else
+                {
+                    return "Second number shouldn't be zero.";
+                }
+            }
+            else
+            {
+                return "Please enter numeric value!";
+            }
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Calculator
 {
-    internal class Program
+    class Program : CalculatorFunctions
     {
         static void Main(string[] args)
         {
@@ -32,101 +32,6 @@ namespace Calculator
             Console.WriteLine(Divide(4, 2));
             Console.WriteLine(Divide("abcded", "Sdfds"));
 
-
-
-
-
-
-        }
-
-        static dynamic Add<t1, t2>(t1 a, t2 b)
-        {
-
-            int n1, n2;
-            double d1, d2;
-            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
-            {
-                return n1 + n2;
-            }
-            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
-            {
-                return d1 + d2;
-            }
-            else
-            {
-                return "Please enter numeric value!";
-            }
-
-        }
-
-
-        static dynamic Subtract<t1, t2>(t1 a, t2 b)
-        {
-            int n1, n2;
-            double d1, d2;
-            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
-            {
-                return n1 - n2;
-            }
-            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
-            {
-                return d1 - d2;
-            }
-            else
-            {
-                return "Please enter numeric value!";
-            }
-        }
-
-        static dynamic Multiply<t1, t2>(t1 a, t2 b)
-        {
-            int n1, n2;
-            double d1, d2;
-            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
-            {
-                return n1 * n2;
-            }
-            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
-            {
-                return d1 * d2;
-            }
-            else
-            {
-                return "Please enter numeric value!";
-            }
-        }
-
-
-        static dynamic Divide<t1, t2>(t1 a, t2 b)
-        {
-            int n1, n2;
-            double d1, d2;
-            if (int.TryParse(a.ToString(), out n1) && int.TryParse(b.ToString(), out n2))
-            {
-                if (n2 != 0)
-                {
-                    return n1 / n2;
-                }
-                else
-                {
-                    return "Second number shouldn't be zero.";
-                }
-            }
-            if (double.TryParse(a.ToString(), out d1) && double.TryParse(b.ToString(), out d2))
-            {
-                if (d2 != 0)
-                {
-                    return d1 / d2;
-                }
-                else
-                {
-                    return "Second number shouldn't be zero.";
-                }
-            }
-            else
-            {
-                return "Please enter numeric value!";
-            }
         }
     }
 }
